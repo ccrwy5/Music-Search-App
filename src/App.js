40de6704946ef2be36051a2f7aae5ebaa1471e3d
Chris Rehagen <ccrwy5@mail.missouri.edu>
2020-07-30 10:57:59 -0500

    Finished top tracks - began lyrics route/component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import Navbar from "./components/layout/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
 
 import { Provider } from "./context";
 
@@ -16,6 +18,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exect path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Lyrics extends Component {
+    render() {
+        return (
+            <div>
+                Lyrics
+            </div>
+        )
+    }
+}
+
+export default Lyrics;
